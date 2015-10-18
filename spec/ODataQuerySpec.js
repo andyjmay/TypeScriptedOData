0382cacf9ccf8047245cd5cfb8be20a2d9813c79
Andy May <andyjmay@gmail.com>
2015-10-18 18:29:05 -0400

    Reorganizes tests

--- spec/ODataQuerySpec.js
+++ spec/ODataQuerySpec.js
@@ -1,20 +1,5 @@
 /// <reference path="../typings/jasmine/jasmine.d.ts"/>
 /// <reference path="../src/ODataQuery.ts"/>
-var TestEntity = (function () {
-    function TestEntity() {
-    }
-    return TestEntity;
-})();
-var RelatedEntity = (function () {
-    function RelatedEntity() {
-    }
-    return RelatedEntity;
-})();
-var AnotherRelatedEntity = (function () {
-    function AnotherRelatedEntity() {
-    }
-    return AnotherRelatedEntity;
-})();
 describe('ODataQuery', function () {
     var odataQuery;
     beforeEach(function () {
@@ -24,66 +9,72 @@ describe('ODataQuery', function () {
         var url = odataQuery.compile();
         expect(url).toBe("");
     });
-    it("should create $select parameter when selecting one property", function () {
-        odataQuery.select(["Name"]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$select=Name");
+    describe("select", function () {
+        it("should create $select parameter when selecting one property", function () {
+            odataQuery.select(["Name"]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$select=Name");
+        });
+        it("should create $select parameter when selecting specific properties of entity", function () {
+            odataQuery.select(["Id", "Name"]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$select=Id,Name");
+        });
     });
-    it("should create $select parameter when selecting specific properties of entity", function () {
-        odataQuery.select(["Id", "Name"]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$select=Id,Name");
+    describe("take", function () {
+        it("should add $top parameter when selecting top x", function () {
+            odataQuery.take(50);
+            var url = odataQuery.compile();
+            expect(url).toBe("$top=50");
+        });
+        it("should handle both select and take", function () {
+            odataQuery.select(["Id", "Name"])
+                .take(50);
+            var url = odataQuery.compile();
+            expect(url).toBe("$select=Id,Name&$top=50");
+        });
     });
-    it("should add $top parameter when selecting top x", function () {
-        odataQuery.take(50);
-        var url = odataQuery.compile();
-        expect(url).toBe("$top=50");
-    });
-    it("should handle both select and take", function () {
-        odataQuery.select(["Id", "Name"])
-            .take(50);
-        var url = odataQuery.compile();
-        expect(url).toBe("$select=Id,Name&$top=50");
-    });
-    it("should add $expand parameter when expanding entity", function () {
-        odataQuery.expand(["Related"]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related");
-    });
-    it("should add $expand parameter when expanding entity", function () {
-        odataQuery.expand(["Related", "AnotherRelated"]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related,AnotherRelated");
-    });
-    it("should add sub-$select query to expand", function () {
-        odataQuery.expand(["Related"], [new TypeScriptedOData.ODataQuery().select(["Title"])]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related($select=Title)");
-    });
-    it("should handle multiple parameters in subquery", function () {
-        odataQuery.expand(["Related", "RelatedCollection"], [new TypeScriptedOData.ODataQuery()
-                .select(["Title"]),
-            new TypeScriptedOData.ODataQuery()
-                .take(5)
-        ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related($select=Title),RelatedCollection($top=5)");
-    });
-    it("should add sub-$select queries to multiple expands", function () {
-        odataQuery.expand(["Related", "AnotherRelated"], [
-            new TypeScriptedOData.ODataQuery().select(["Title"]),
-            new TypeScriptedOData.ODataQuery().select(["Status"])
-        ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related($select=Title),AnotherRelated($select=Status)");
-    });
-    it("should handle one null query with multiple expands", function () {
-        odataQuery.expand(["Related", "AnotherRelated"], [
-            null,
-            new TypeScriptedOData.ODataQuery().select(["Status"])
-        ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related,AnotherRelated($select=Status)");
+    describe("expand", function () {
+        it("should add $expand parameter when expanding entity", function () {
+            odataQuery.expand(["Related"]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related");
+        });
+        it("should add $expand parameter when expanding entity", function () {
+            odataQuery.expand(["Related", "AnotherRelated"]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related,AnotherRelated");
+        });
+        it("should add sub-$select query to expand", function () {
+            odataQuery.expand(["Related"], [new TypeScriptedOData.ODataQuery().select(["Title"])]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related($select=Title)");
+        });
+        it("should handle multiple parameters in subquery", function () {
+            odataQuery.expand(["Related", "RelatedCollection"], [new TypeScriptedOData.ODataQuery()
+                    .select(["Title"]),
+                new TypeScriptedOData.ODataQuery()
+                    .take(5)
+            ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related($select=Title),RelatedCollection($top=5)");
+        });
+        it("should add sub-$select queries to multiple expands", function () {
+            odataQuery.expand(["Related", "AnotherRelated"], [
+                new TypeScriptedOData.ODataQuery().select(["Title"]),
+                new TypeScriptedOData.ODataQuery().select(["Status"])
+            ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related($select=Title),AnotherRelated($select=Status)");
+        });
+        it("should handle one null query with multiple expands", function () {
+            odataQuery.expand(["Related", "AnotherRelated"], [
+                null,
+                new TypeScriptedOData.ODataQuery().select(["Status"])
+            ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related,AnotherRelated($select=Status)");
+        });
     });
     describe("$count", function () {
         it("should default $count to true when calling count()", function () {
--- spec/ODataQuerySpec.ts
+++ spec/ODataQuerySpec.ts
@@ -1,25 +1,6 @@
 /// <reference path="../typings/jasmine/jasmine.d.ts"/>
 /// <reference path="../src/ODataQuery.ts"/>
 
-class TestEntity {
-    Id: number;
-    Name: string;
-    Created: Date;
-    Related: RelatedEntity;
-    AnotherRelated: AnotherRelatedEntity;
-    RelatedCollection: RelatedEntity[];
-}
-
-class RelatedEntity {
-    Id: number;
-    Title: string;
-}
-
-class AnotherRelatedEntity {
-    Id: number;
-    Status: string;
-}
-
 describe('ODataQuery', () => {
     var odataQuery: TypeScriptedOData.ODataQuery;
     
@@ -31,82 +12,88 @@ describe('ODataQuery', () => {
         var url = odataQuery.compile();
         expect(url).toBe("");
     });
-    
-    it("should create $select parameter when selecting one property", () => {
-        odataQuery.select([ "Name" ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$select=Name");
+
+    describe("select", () => {
+        it("should create $select parameter when selecting one property", () => {
+            odataQuery.select([ "Name" ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$select=Name");
+        });
+
+        it("should create $select parameter when selecting specific properties of entity", () => {
+            odataQuery.select([ "Id", "Name" ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$select=Id,Name");
+        });
     });
 
-    it("should create $select parameter when selecting specific properties of entity", () => {
-        odataQuery.select([ "Id", "Name" ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$select=Id,Name");
+    describe("take", () => {
+        it("should add $top parameter when selecting top x", () => {
+            odataQuery.take(50);
+            var url = odataQuery.compile();
+            expect(url).toBe("$top=50");
+        });
+
+        it("should handle both select and take", () => {
+            odataQuery.select([ "Id", "Name" ])
+                    .take(50);
+            var url = odataQuery.compile();
+            expect(url).toBe("$select=Id,Name&$top=50");
+        });
     });
-    
-    it("should add $top parameter when selecting top x", () => {
-        odataQuery.take(50);
-        var url = odataQuery.compile();
-        expect(url).toBe("$top=50");
+
+    describe("expand", () => {
+        it("should add $expand parameter when expanding entity", () => {
+            odataQuery.expand([ "Related" ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related");
+        });
+
+        it("should add $expand parameter when expanding entity", () => {
+            odataQuery.expand([ "Related", "AnotherRelated" ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related,AnotherRelated");
+        });
+
+        it("should add sub-$select query to expand", () => {
+            odataQuery.expand([ "Related" ],
+                [ new TypeScriptedOData.ODataQuery().select([ "Title" ]) ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related($select=Title)");
+        });
+
+        it("should handle multiple parameters in subquery", () => {
+            odataQuery.expand([ "Related", "RelatedCollection" ],
+                [ new TypeScriptedOData.ODataQuery()
+                    .select([ "Title" ]),
+                new TypeScriptedOData.ODataQuery()
+                    .take(5)
+                ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related($select=Title),RelatedCollection($top=5)");
+        });
+
+        it("should add sub-$select queries to multiple expands", () => {
+            odataQuery.expand([ "Related", "AnotherRelated" ],
+                [ 
+                new TypeScriptedOData.ODataQuery().select([ "Title" ]),
+                new TypeScriptedOData.ODataQuery().select([ "Status" ])
+                ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related($select=Title),AnotherRelated($select=Status)");
+        });
+
+        it("should handle one null query with multiple expands", () => {
+            odataQuery.expand([ "Related", "AnotherRelated" ],
+                [ 
+                null,
+                new TypeScriptedOData.ODataQuery().select([ "Status" ])
+                ]);
+            var url = odataQuery.compile();
+            expect(url).toBe("$expand=Related,AnotherRelated($select=Status)")
+        });
     });
-    
-    it("should handle both select and take", () => {
-        odataQuery.select([ "Id", "Name" ])
-                  .take(50);
-        var url = odataQuery.compile();
-        expect(url).toBe("$select=Id,Name&$top=50");
-    });
-    
-    it("should add $expand parameter when expanding entity", () => {
-        odataQuery.expand([ "Related" ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related");
-    });
-    
-    it("should add $expand parameter when expanding entity", () => {
-        odataQuery.expand([ "Related", "AnotherRelated" ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related,AnotherRelated");
-    });
-    
-    it("should add sub-$select query to expand", () => {
-        odataQuery.expand([ "Related" ],
-            [ new TypeScriptedOData.ODataQuery().select([ "Title" ]) ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related($select=Title)");
-    });
-    
-    it("should handle multiple parameters in subquery", () => {
-        odataQuery.expand([ "Related", "RelatedCollection" ],
-            [ new TypeScriptedOData.ODataQuery()
-                .select([ "Title" ]),
-              new TypeScriptedOData.ODataQuery()
-                .take(5)
-            ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related($select=Title),RelatedCollection($top=5)");
-    });
-    
-    it("should add sub-$select queries to multiple expands", () => {
-        odataQuery.expand([ "Related", "AnotherRelated" ],
-            [ 
-              new TypeScriptedOData.ODataQuery().select([ "Title" ]),
-              new TypeScriptedOData.ODataQuery().select([ "Status" ])
-            ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related($select=Title),AnotherRelated($select=Status)");
-    });
-    
-    it("should handle one null query with multiple expands", () => {
-        odataQuery.expand([ "Related", "AnotherRelated" ],
-            [ 
-              null,
-              new TypeScriptedOData.ODataQuery().select([ "Status" ])
-            ]);
-        var url = odataQuery.compile();
-        expect(url).toBe("$expand=Related,AnotherRelated($select=Status)")
-    });
-    
+
     describe("$count", () => {
         it("should default $count to true when calling count()", () => {
             odataQuery.count();
